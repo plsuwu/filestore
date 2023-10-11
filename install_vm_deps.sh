6bdec207c0386d32d96057ef818f9d95da5bf783
pls <zac.womal@outlook.com>
2023-10-12 02:45:31 +1000

    Update install_vm_deps.sh
    fix inconsistencies, suppress interactive prompts

--- install_vm_deps.sh
+++ install_vm_deps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
-apt install git -y
+sudo apt install -y git
 
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y --no-install-recommends ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -12,11 +12,11 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 sudo apt-get update
+sudo apt-get install -y -q --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-mkdir /home/pls/repos
+mkdir -p /home/pls/repos # likely wont exist in a fresh vm instance anyway
 cd /home/pls/repos
 git clone -b gcloud https://github.com/plsuwu/filestore.git
 cd filestore
