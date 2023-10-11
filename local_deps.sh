1c1cd6001909fa5a8a3af2323e06d011f8994095
pls <zac.womal@outlook.com>
2023-10-12 02:41:51 +1000

    Update local_deps.sh
    suppress prompts

--- local_deps.sh
+++ local_deps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
-apt install git -y
+apt install -y git
 
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install  -y --no-install-recommends ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -12,11 +12,12 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 sudo apt-get update
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt-get install -y -q --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-mkdir /home/pls/repos
+mkdir -p /home/pls/repos
 cd /home/pls/repos
 git clone https://github.com/plsuwu/filestore.git
 cd filestore
